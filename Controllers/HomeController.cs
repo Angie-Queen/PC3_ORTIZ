060876999e096fdeaa3c65e96b591f4c201ee9c2
Angie Ortiz <angieortizavalos@gmail.com>
2020-12-13 16:03:20 -0500

    funcionalidad ok

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,17 +12,41 @@ namespace PC3_ORTIZ.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly WebAppContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, WebAppContext context)
         {
             _logger = logger;
+            _context=context;
         }
 
         public IActionResult Index()
         {
+            var productos=_context.Productos.ToList();
+            return View(productos);
+        }
+        [HttpPost]
+        public IActionResult Eliminar(int id){
+
+            var productos = _context.Productos.FirstOrDefault(x => x.id == id);
+            _context.Remove(productos);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Productos()
+        {
             return View();
         }
-
+        [HttpPost]
+        public IActionResult Productos(Productos p)
+        {
+            if(ModelState.IsValid){
+                _context.Add(p);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
         public IActionResult Privacy()
         {
             return View();
